43eeed908ad156cd6de19201ba345b13a980e6ed
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2022-06-02 17:06:52 -0400

    testing deploy container script

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 const commonConfig = require('./webpack.common')
@@ -21,9 +20,6 @@ const devConfig = {
         './MarketingApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
 }
