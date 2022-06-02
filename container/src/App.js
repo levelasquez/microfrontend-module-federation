d275f71d71a3a181a8d17a27ec92b9eb21da9b3a
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2022-06-02 14:56:33 -0400

    initial commit

---
+++ container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import MarketingApp from './components/MarketingApp'
+
+export default () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  )
+}
---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+const mount = (element) => {
+  ReactDOM.render(<App />, element)
+}
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root')
+
+  if (devRoot) {
+    mount(devRoot)
+  }
+}
+
+export { mount }
