ce89ccc05d35548d7728d20129631e0c4f0d3941
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2022-06-03 12:18:18 -0400

    update title

--- container/src/App.js
+++ container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp'
 export default () => {
   return (
     <div>
-      <h1>Hi there!</h1>
+      <h1>Hi there from Container!</h1>
       <hr />
       <MarketingApp />
     </div>
