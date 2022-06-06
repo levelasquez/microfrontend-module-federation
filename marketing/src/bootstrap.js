26f82b69de805acdfa92acd9537a749c42b0e8e6
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2022-06-06 14:36:07 -0400

    Implementing multi-tier navigation

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createMemoryHistory, createBrowserHistory } from 'history'
 
 import App from './App'
 
-const mount = (element) => {
-  ReactDOM.render(<App />, element)
+const mount = (element, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory()
+
+  if (onNavigate) {
+    history.listen(onNavigate)
+  }
+
+  ReactDOM.render(<App history={history} />, element)
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname)
+      }
+    },
+  }
 }
 
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root')
 
   if (devRoot) {
-    mount(devRoot)
+    mount(devRoot, { defaultHistory: createBrowserHistory() })
   }
 }
 
