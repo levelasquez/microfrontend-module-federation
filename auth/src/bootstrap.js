212b4a97e316655d9f17b55a673f94644e86c924
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2022-06-13 13:41:20 -0400

    Authentication in microfrontends

--- auth/src/bootstrap.js
+++ auth/src/bootstrap.js
@@ -4,7 +4,10 @@ import { createMemoryHistory, createBrowserHistory } from 'history'
 
 import App from './App'
 
-const mount = (element, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (
+  element,
+  { onSignIn, onNavigate, defaultHistory, initialPath },
+) => {
   const history =
     defaultHistory ||
     createMemoryHistory({
@@ -15,7 +18,7 @@ const mount = (element, { onNavigate, defaultHistory, initialPath }) => {
     history.listen(onNavigate)
   }
 
-  ReactDOM.render(<App history={history} />, element)
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, element)
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
