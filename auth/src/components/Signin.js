7e6faf0eb11c20e02dda8e0b5695ae6fc45a1ad3
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2022-06-13 13:45:35 -0400

    fix

--- auth/src/components/Signin.js
+++ auth/src/components/Signin.js
@@ -17,7 +17,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" to="/">
-        Your Website
+        Your Websitee
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
